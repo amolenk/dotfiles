3d45ac97cbb1e6737bb35b78cff37760e2d08c03
Sander Molenkamp <a.molenkamp@gmail.com>
2023-07-09 20:33:32 +0200

    Brewfile

---
+++ MacAppStore.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mas install 824183456  # Affinity Photo
+mas install 824171161  # Affinity Designer
+mas install 1295203466 # Microsoft Remote Desktop
+mas install 441258766  # Magnet
+mas install 1514817810 # Poolsuite FM
+mas install 904280696  # Things
+mas install 1529448980 # Reeder
+mas install 961632517  # Be Focused Pro
+mas install 524141863  # Jump Desktop
--- homebrew.sh
+++ homebrew.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+# check if environment parameter is passed
+if [ $# -eq 0 ]
+then
+    echo "Environment parameter is missing."
+    exit 1;
+fi
+
+# check if environment is valid
+if [ "$1" != "home" ] && [ "$1" != "work" ] && [ "$1" != "workmin" ]
+then
+    echo "Invalid environment parameter, must be one of 'home', 'work' or 'workmin'"
+    exit 1
+fi
  
 if test ! $(which brew); then
     echo "Installing homebrew..."
@@ -6,4 +20,5 @@ if test ! $(which brew); then
 fi
 
 brew update
-brew bundle
+brew bundle --file Brewfile.essentials
+##brew bundle --file "$1"
