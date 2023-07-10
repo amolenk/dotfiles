41c3cf68dfab3bbcc852862efc60ca9a96d78731
Sander Molenkamp <a.molenkamp@gmail.com>
2023-07-10 19:39:09 +0200

    Mac apps

--- MacAppStore.essentials.sh
+++ MacAppStore.essentials.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-mas install 441258766  # Magnet
-mas install 904280696  # Things
 mas install 961632517  # Be Focused Pro
 mas install 524141863  # Jump Desktop
+mas install 441258766  # Magnet
+mac install 1549370672 # Save to Raindrop.io
+mas install 904280696  # Things
+mac install 1607635845 # Velja browser picker
+
