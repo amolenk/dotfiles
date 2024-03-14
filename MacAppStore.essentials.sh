0a7bd10629050928a2620d3aab54fa8823f26bfe
Sander Molenkamp <sander@sandermolenkamp.com>
2024-03-14 08:58:42 +0100

    Updates for new MBP

--- MacAppStore.essentials.sh
+++ MacAppStore.essentials.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 mas install 961632517  # Be Focused Pro
-mas install 524141863  # Jump Desktop
 mas install 904280696  # Things
 mas install 1607635845 # Velja browser picker
 mas install 1091189122 # Bear
-mas install 1593408455 # Anybox
+mas install 1514817810 # Poolsuite FM
+mas install 441258766 # Magnet
+#mas install 524141863  # Jump Desktop
+#mas install 1593408455 # Anybox
--- MacAppStore.work.sh
+++ MacAppStore.work.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-mas install 1514817810 # Poolsuite FM
 mas install 1295203466 # Microsoft Remote Desktop
