6e7f9a4a27ab6db8c28e5f0122ebc14be8e4178a
Sander Molenkamp <sander@sandermolenkamp.com>
2024-03-23 09:29:19 +0100

    Add Magnet and Citrix

--- MacAppStore.essentials.sh
+++ MacAppStore.essentials.sh
@@ -4,6 +4,7 @@
 mas install 961632517  # Be Focused Pro
 mas install 1091189122 # Bear
 #mas install 524141863  # Jump Desktop
+mas install 441258766  # Magnet
 mas install 1295203466 # Microsoft Remote Desktop
 mas install 1514817810 # Poolsuite FM
 mas install 1529448980 # Reeder
